013535ef385b722e5f8b49578c6492923a4529c6
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-10-18 13:44:50 +0200

    add EOF and rename functions

--- Hangman.py
+++ Hangman.py
@@ -2,15 +2,15 @@ from HangmanStages import stages
 from Words import word_list
 import random
 
-def printHangmanStages(stage):
+def print_hangman_stages(stage):
     print(stages[stage])
 
-def getRandomWord():
+def get_random_word():
     word = random.choice(word_list).upper()
 
     return word
 
-def getWordIndices(word, guess):
+def get_word_indices(word, guess):
     indices = []
 
     for index, letter in enumerate(word):
@@ -20,9 +20,9 @@ def getWordIndices(word, guess):
     
     return indices
 
-def fillLettersInWord(word_completion, word, guess):
+def fill_letters_in_word(word_completion, word, guess):
     word_as_list = list(word_completion)
-    indices = getWordIndices(word, guess)
+    indices = get_word_indices(word, guess)
 
     for index in indices:
         word_as_list[index] = guess
@@ -36,19 +36,19 @@ def play(word):
     word_was_guessed = False
     hidden_letter = "_"
     word_completion = hidden_letter * len(word)
-    listOfLetters = [0] * remaining_tries
+    list_of_letters = [0] * remaining_tries
 
-    printHangmanStages(6)
+    print_hangman_stages(6)
     print(word_completion)
 
     while not word_was_guessed and remaining_tries > 0:
         guess = input("Buchstabe oder Wort eingeben: ").upper()
 
         if guess not in word:
-            if guess in listOfLetters:
+            if guess in list_of_letters:
                 print("Du hast " + guess + " schon einmal eingegeben.")
             else:
-                listOfLetters[7 - remaining_tries] = guess
+                list_of_letters[7 - remaining_tries] = guess
                 print(f"Das gesuchte Wort enthält den Buchstaben " + guess + " nicht.")
                 remaining_tries -= 1
         else:
@@ -58,11 +58,11 @@ def play(word):
                 break
             else:
                 print(f"Das gesuchte Wort enthält den Buchstaben " + guess + ".")
-                word_completion = fillLettersInWord(word_completion, word, guess)
+                word_completion = fill_letters_in_word(word_completion, word, guess)
                 word_was_guessed = hidden_letter not in word_completion
 
         stage = remaining_tries - 1
-        printHangmanStages(stage)
+        print_hangman_stages(stage)
         print(word_completion)
 
     if word_was_guessed:
@@ -70,5 +70,5 @@ def play(word):
     elif remaining_tries == 0:
         print("Schade. Das gesuchte Wort war: " + word)
 
-word = getRandomWord()
+word = get_random_word()
 play(word)
